fe609b5721fdd3bc555d7aac557300e19765e536
Cindrom1 <cindrof@gmail.com>
2023-02-26 16:36:18 +0300

    add Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите значение переменной a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение переменной b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > b) 
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+Console.Write("max = ");
+Console.Write(max);
