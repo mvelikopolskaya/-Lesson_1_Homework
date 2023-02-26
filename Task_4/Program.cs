f9809da2e78bebafc4d3a73ac3cbf0208420e5f7
Cindrom1 <cindrof@gmail.com>
2023-02-26 20:12:46 +0300

    add Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите переменную a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите переменную b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите переменную c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if ((a > b) && (a > c)) max = a;
+else if ((b > a) && (b > c)) max = b;
+else
+{
+    max = c;
+}
+Console.WriteLine(max);
