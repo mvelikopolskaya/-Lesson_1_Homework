e9b75794903c44a362e4b2e9ab370e9d17994b6d
Cindrom1 <cindrof@gmail.com>
2023-02-26 19:47:33 +0300

    add Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите значение переменной a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+int number = 1;
+
+while (number < a)
+{
+    if (number % 2 == 0) 
+        Console.WriteLine(number);
+    number++;
+}
