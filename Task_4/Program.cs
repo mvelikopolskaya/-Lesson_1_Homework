069fe5bb019defce38e9f9324179c3f826def852
Cindrom1 <cindrof@gmail.com>
2023-02-26 17:22:49 +0300

    add Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите значение переменной a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение переменной b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение переменной c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if ()
+{
+
+}
+else
+{
+    
+}
---
+++ Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите значение переменной a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine($"Число {a} - четное");
+}
+else
+{
+    Console.WriteLine($"Число {a} - нечетное");
+}
